6fe54bebf43ef078faf7387a01161d355ba4332a
bassy1992 <wyarquah@gmail.com>
2025-11-04 09:24:06 +0000

    Fix admin CSRF cookie properly

--- back/config/admin.py
+++ back/config/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.utils.decorators import method_decorator
 
-# Force CSRF cookie on admin login
-admin.site.login = method_decorator(ensure_csrf_cookie)(admin.site.login)
+# Custom AdminSite that ensures CSRF cookie
+class CustomAdminSite(AdminSite):
+    @method_decorator(ensure_csrf_cookie)
+    def login(self, request, extra_context=None):
+        return super().login(request, extra_context)
+
+# Replace default admin site
+admin.site.__class__ = CustomAdminSite
