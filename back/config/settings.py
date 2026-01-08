12e41d09e251b57f4b4dd87aaf4491f126337d34
bassy1992 <wyarquah@gmail.com>
2026-01-08 15:25:23 +0000

    Fix: Make cloudinary optional - only load if module is available

--- back/config/settings.py
+++ back/config/settings.py
@@ -82,8 +82,6 @@ INSTALLED_APPS = [
     'corsheaders',
     'drf_spectacular',
     'storages',
-    'cloudinary_storage',
-    'cloudinary',
     
     # Local apps
     'apps.products',
@@ -221,13 +219,19 @@ CLOUDINARY_CLOUD_NAME = config('CLOUDINARY_CLOUD_NAME', default=None)
 CLOUDINARY_API_KEY = config('CLOUDINARY_API_KEY', default=None)
 CLOUDINARY_API_SECRET = config('CLOUDINARY_API_SECRET', default=None)
 
+# Try to use Cloudinary if configured and available
 if CLOUDINARY_CLOUD_NAME:
-    CLOUDINARY_STORAGE = {
-        'CLOUD_NAME': CLOUDINARY_CLOUD_NAME,
-        'API_KEY': CLOUDINARY_API_KEY,
-        'API_SECRET': CLOUDINARY_API_SECRET,
-    }
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    try:
+        import cloudinary_storage
+        CLOUDINARY_STORAGE = {
+            'CLOUD_NAME': CLOUDINARY_CLOUD_NAME,
+            'API_KEY': CLOUDINARY_API_KEY,
+            'API_SECRET': CLOUDINARY_API_SECRET,
+        }
+        DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+        INSTALLED_APPS += ['cloudinary_storage', 'cloudinary']
+    except ImportError:
+        pass  # Cloudinary not installed, skip
 
 # Railway S3-compatible storage for media files (alternative option)
 AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL', default=None)
@@ -242,8 +246,8 @@ AWS_QUERYSTRING_AUTH = True  # Use signed URLs for private bucket
 AWS_S3_URL_PROTOCOL = 'https:'
 AWS_QUERYSTRING_EXPIRE = 3600  # URLs expire in 1 hour
 
-# Use S3 if configured and Cloudinary is not
-if AWS_S3_ENDPOINT_URL and not CLOUDINARY_CLOUD_NAME:
+# Use S3 if configured and Cloudinary is not set up
+if AWS_S3_ENDPOINT_URL and 'cloudinary_storage' not in INSTALLED_APPS:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/'
 
